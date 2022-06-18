f5a3ebc17726b829ca22fff6aaf476223c650876
shivam256 <shivamgavandi256@gmail.com>
2022-06-18 09:45:57 +0530

    changes axios baseUrl

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:5000'
+    baseURL:'https://lostandfound-api.herokuapp.com'
+    //baseUrl:'http://localhost:5000'
 })
 
 
